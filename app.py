5dbedd09c8364c09ca6b5c51dd82f28f1aee1d21
Trupti Mane <trupti@intern.boll-und-partner.com>
2021-04-28 14:22:16 +0200

    My First Commit

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
+print("Hello, World!")
+st.title('My first app 12')
